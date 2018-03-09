632ec6a8b48d04cc11fd63594fbb16b9242ee799
Steven Torres <stevetorres7562@gmail.com>
2018-03-09 23:31:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+    const two = 2
+ 
 
   // Feel free to move things around!
-  const two = 2
+   const two = 2
 }
 
 var funkyFunction = function() {
